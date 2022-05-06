a9ac68d20b00b285304aafbc8637c3eba1670e09
Matthias <m@tth.es>
2022-05-06 19:08:16 +0200

    update

--- www/cjs/active-button.js
+++ www/cjs/active-button.js
@@ -14,10 +14,14 @@ class ActiveButton {
      */
     #button;
     #spinner;
-    constructor(elem, onclick=null) {
+    #origElement;
+    #optClasses;
+
+    constructor(elem, onclick=null, optClasses="") {
         if ( ! (elem instanceof HTMLElement)) {
             elem = KaToolsV1.querySelector(elem);
         }
+        this.#optClasses = optClasses;
         this.#button = elem;
         if (onclick)
             this.#button.addEventListener("click", onclick);
@@ -34,18 +38,36 @@ class ActiveButton {
         this.spinner(false);
     }
 
+    click() {
+        this.#button.dispatchEvent(new Event("click"));
+    }
+
     spinner(active = true) {
         if(active) {
-            this.#spinner = document.createElement("span");
-            this.#spinner.setAttribute("class", "spinner-border spinner-border-sm");
-            this.#button.appendChild(this.#spinner);
+            if (this.#button.querySelector("[role='status']")) {
+                this.#spinner = this.#button.querySelector("[role='status']").cloneNode(true);
+                this.#spinner.removeAttribute("hidden");
+            } else {
+                this.#spinner = document.createElement("span");
+                this.#spinner.setAttribute("class", "spinner-border spinner-border-sm");
+            }
+            let origIcon = this.#button.querySelector("i");
+            if (origIcon !== null) {
+                this.#origElement = origIcon.cloneNode(true);
+                origIcon.replaceWith(this.#spinner);
+            } else {
+                this.#button.appendChild(this.#spinner);
+            }
             return;
         }
         if (this.#spinner) {
+            if (this.#origElement !== null) {
+                this.#spinner.replaceWith(this.#origElement)
+                return;
+            }
             this.#button.removeChild(this.#spinner);
             this.#spinner = null;
         }
-
     }
 
 }
